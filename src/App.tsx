1882429d7dfa1d39d44e60a47050bf7e3e74c4d1
Jayathu Amarasinghe <jayathuamarasinghe@gmail.com>
2026-04-23 12:17:17 +0930

    Adding initial loading suspense and main component.

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,18 @@
-import './App.css'
+import { Suspense } from "react";
+import "./App.css";
+import EmployeeList from "./components/EmployeeList";
 
 function App() {
-
   return (
     <>
-      <div className='text-red-300'>test</div>
+      <div className="font-bold text-3xl text-blue-500 my-5">
+        Employees System
+      </div>
+      <Suspense fallback={<div>Initial Loading....</div>}>
+        <EmployeeList />
+      </Suspense>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
